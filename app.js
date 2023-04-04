4635fc55b786440be7b061b3a68ff5de034da3d1
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-04-04 17:34:43 +0530

    post route

--- app.js
+++ app.js
@@ -31,6 +31,24 @@ app.get('/articles', function(req, res) {
     });
 })
 
+app.post('/articles', function(req, res) {
+    console.log(req.body.title)  
+    console.log(req.body.content)  
+
+    const newArticle = new Article({
+        title : req.body.title,
+        content: req.body.content
+    })
+
+    newArticle.save().then(function(err) {
+        if(!err) {
+            res.send("successfully added a new article")
+        }else{
+            res.send(err);
+        }
+    })
+})
+
 
 
 app.listen(3000, function(){
